803e9b5decd733400dae069efc52da7dcb72a029
DiegoUwUwU <82970286+DiegoUwUwU@users.noreply.github.com>
2021-11-26 04:12:23 -0600

    Se agregan las opciones de suma y conversión de Km a M

--- app/src/main/java/com/calculadoravelt/MainActivity.java
+++ app/src/main/java/com/calculadoravelt/MainActivity.java
@@ -2,13 +2,41 @@ package com.calculadoravelt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSuma;
+    private Button buttonKmaM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSuma = findViewById(R.id.buttonSuma );
+        buttonSuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirSuma( );
+            }
+        });
+        buttonKmaM = findViewById(R.id.buttonKmaM );
+        buttonKmaM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirConversionKm( );
+            }
+        });
+    }
+    private void abrirSuma() {
+        Intent intent = new Intent( this , SumaActivity.class );
+        startActivity( intent );
+    }
+    private void abrirConversionKm() {
+        Intent intent = new Intent( this , KmActivity.class );
+        startActivity( intent );
     }
 }
